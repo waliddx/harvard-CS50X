d045da4ab87d9211dec2963328cea8ee999be1eb
walid <wagzouly@gmail.com>
2024-08-16 20:22:37 +0100

    reaf: time efficient

--- week1/cash/cash.c
+++ week1/cash/cash.c
@@ -1,35 +1,38 @@
 #include <stdio.h>
 
+int change_owed(int budget);
 int main(void)
+{
+	int cash;
+	do
+	{
+	printf("Change owed: ");
+	scanf("%i", &cash);
+	} while (cash < 0);
+
+	int coin = change_owed(cash);
+	printf("%i\n", coin);
+}
+
+int change_owed(int budget)
 {
 	int twenty_five = 25;
 	int ten = 10;
 	int five = 5;
 	int one = 1;
+
 	int count = 0;
 
-	int cash;
-	printf("Change owed: ");
-	scanf("%i", &cash);
-	while (cash >= twenty_five)
-	{
-		cash -= 25;
-		count++;
-	}
-	while (cash >= ten)
-    {
-        cash -= 10;
-		count++;
-    }
-	while (cash >= five)
-    {
-		cash -= 5;
-		count++;
-    }
-	while (cash >= one)
-    {
-		cash -= 1;
-		count++;
-    }
-	printf("%i", count);
+	count += budget / twenty_five;
+	budget %= twenty_five;
+
+	count += budget / ten;
+	budget %= ten;
+
+	count += budget / five;
+	budget %= five;
+
+	count += budget / one;
+
+	return count;
 }
